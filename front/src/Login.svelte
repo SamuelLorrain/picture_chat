3ffba77c061ad98ce3465b7179d2c6d3c12037bd
SamuelLorrain <samuel.lorrain@gmail.com>
2022-06-10 22:45:42 +0200

    begin router

--- front/src/Login.svelte
+++ front/src/Login.svelte
@@ -1,20 +1,32 @@
-<template>
-    <form>
-        <label>
-            Username
-            <input type="text" name="username" value={username}/>
-        </label>
-        <label>
-            Password
-            <input type="password" name="password" value={password}/>
-        </label>
-    </form>
+<Router>
+    <div>
+        <Link to="/">Link</Link>
+        <Link to="/chat">Chat</Link>
 
-</template>
+        <Route path="/">
+            <form>
+                <label>
+                    Username
+                    <input type="text" name="username" value={username}/>
+                </label>
+                <label>
+                    Password
+                    <input type="password" name="password" value={password}/>
+                </label>
+            </form>
+        </Route>
+        <Route path="/chat">
+            <Chat/>
+        </Route>
+    </div>
+</Router>
 
 <script>
-    let username;
-    let password;
+    import { Link, Route, Router } from 'svelte-navigator';
+    import Chat from './Chat.svelte';
+
+    let username = '';
+    let password = '';
     function login() {
         let token = fetch('http://localhost:3000/tokens/', {
             method: 'POST',
